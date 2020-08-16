95c0be250b8ff8cc91870e0c8221e08a4b5fb0ac
SubhanshuM <shekhar.subhanshu@gmail.com>
2020-08-16 23:21:05 +0530

    closes #2

---
+++ DataTypeExamples.py
@@ -0,0 +1,29 @@
+# Fundamental DataTypes
+# 1. int
+print(2 + 4.0)
+print(type(2 + 4.0))
+print(type(6))
+print(type(2 - 4))
+print(2 / 6)
+print(type(2 / 6))
+print(2 * 6)
+print(type(2 * 6))
+print(2 ** 6)
+print((type(2 ** 6)))
+print(3 // 4)
+print(5 // 4)
+print(5 % 4)
+print(round(3.3))
+print(round(3.9))
+print(abs(-20))
+# 2. float
+# 3. bool
+# 4. str
+# 5. list
+# 6. tuple
+# 7. set
+# 8. dict
+
+# Casses Custom TYpes
+# Sepecialized Data Types
+# None#
